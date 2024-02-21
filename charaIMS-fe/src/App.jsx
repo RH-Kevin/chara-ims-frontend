d184088538b9342638f3f4da8bc259dec874bb93
RH-Kevin <147596439+RH-Kevin@users.noreply.github.com>
2024-02-21 12:09:58 +0900

    add:basic login

--- charaIMS-fe/src/App.jsx
+++ charaIMS-fe/src/App.jsx
@@ -11,21 +11,29 @@ import AddDevice from './components/AddRecord';
 
 
 export default function App() {
-  const [showLogin, setShowLogin] = useState(false);
+
+
+
+  const [showLogin, setShowLogin] = useState(true);
   const [showDashboard, setShowDashboard] = useState(false);
   const [showDeviceList, setShowDeviceList] = useState(false);
   const [showGroups, setShowGroups] = useState(false);
   const [showLog, setShowLog] = useState(false);
   const [showQR, setShowQR] = useState(false);
   const [showAddDevice, setShowAddDevice] = useState(false);
-  const [showSideNavbar, setShowSideNavbar] = useState(true);
+  const [showSideNavbar, setShowSideNavbar] = useState(false);
 
-
+  const handleLoginSuccess = () => {
+    setShowLogin(false); // Hide the login component
+    setShowDashboard(true); // Show the dashboard as the initial view after login
+    setShowSideNavbar(true); // Show the side navbar after login
+    // Update other state as necessary based on your app's flow
+  };
 
 
   return (
     <>
-      {showLogin && <Login/>}
+      {showLogin && <Login onLoginSuccess={handleLoginSuccess} />}
       {showSideNavbar && <SideNavbar setShowLogin={setShowLogin} setShowDashboard={setShowDashboard} setShowDeviceList={setShowDeviceList} setShowGroups={setShowGroups} setShowLog={setShowLog} setShowQR={setShowQR} setShowAddDevice={setShowAddDevice}/>}
       {showDashboard && <Dashboard/>}
       {showDeviceList && <DeviceList/>}
